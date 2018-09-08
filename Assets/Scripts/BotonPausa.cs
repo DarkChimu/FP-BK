be7f67f273dd747d0616b488f21cc26261dfb581
Unity64 <unitydeveloper64@gmail.com>
2018-09-08 10:30:38 +0200

    cambios en el boton tienda, menu de pausa implementado en ambos modos, fondo ajustado a la pantalla siempre y he añadido las imagenes de monedas de revivir en la tienda

---
+++ Assets/Scripts/BotonPausa.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonPausa : MonoBehaviour {
+
+
+	public GameObject panelDePausa;
+
+
+	public void PonerPausa(){
+		panelDePausa.SetActive (true);
+		Time.timeScale = 0;
+	}
+
+
+	public void QuitarPausa(){
+		panelDePausa.SetActive (false);
+		Time.timeScale = 1;
+	}
+}
