3a662952f177f4f783a0139d4f1e9b6144dc04ef
Darkie64 <darkchimu@hotmail.com>
2018-09-10 10:03:21 -0400

    Boton de salida e implementando nuevo cargador de niveles

---
+++ Assets/Scripts/Cargador.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Cargador : MonoBehaviour {
+
+	public void VolverMenu(){
+		SceneManager.LoadScene("EscenaMenu", LoadSceneMode.Single);
+	}
+
+	public void CerrarJuego(){
+		Application.Quit();
+		Debug.Log("Aplicaccion Cerrada");
+	}
+
+
+}
