836019f7d216b6a3f45996f5314e3158fb4438ae
Unity64 <unitydeveloper64@gmail.com>
2018-09-24 12:59:01 +0200

    introduccion de npcs aleatorios, barras de sonido y portal al mapa de juego, seguir al jugador con la camara en la escena del bar

---
+++ Assets/Teletransporte.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teletransporte : MonoBehaviour {
+
+
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")  {
+            SceneManager.LoadScene("EscenaMapaHistoria", LoadSceneMode.Single);
+          
+        }
+    }
+
+
+    
+}
