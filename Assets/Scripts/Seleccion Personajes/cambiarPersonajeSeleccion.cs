9b55c0ac00fa6a784533f8ebbb78a4c71ae54ad5
Unity64 <unitydeveloper64@gmail.com>
2018-10-01 10:26:19 +0200

    seleccion de personajes

--- Assets/Scripts/Seleccion Personajes/cambiarPersonajeSeleccion.cs
+++ Assets/Scripts/Seleccion Personajes/cambiarPersonajeSeleccion.cs
@@ -18,38 +18,23 @@ public class cambiarPersonajeSeleccion : MonoBehaviour {
 
 
 	//PERSONAJES 
-
-	//PERSONAJE1
 	public GameObject Personaje1;
-
-
-	//PERSONAJE2
 	public GameObject Personaje2;
-
-
-
-	//PERSONAJE3
 	public GameObject Personje3;
-
-
-
-	//PERSONAJE4 
 	public GameObject Personaje4;
-
-
-
-	//PERSONAJE5
 	public GameObject Personaje5;
 
 
 
+    public GameObject flechaMas;
+    public GameObject flechaMenos;
 
 
 
 
 
 
-	void Update(){
+    void Update(){
 
 
 		personajeSeleccionDerechaB = FlechaDerechaClick.personajeSeleccionDerecha;
@@ -68,65 +53,72 @@ public class cambiarPersonajeSeleccion : MonoBehaviour {
 
 
 
-		//PERSONAJE1
-		if (NuneroDePersonajeSeleccionado == 1) {
-			Personaje1.SetActive (true);
-		
-
-		} else {
-			Personaje1.SetActive (false);
-		
-
-		}
+        //PERSONAJE1
+        if (NuneroDePersonajeSeleccionado == 1)
+        {
+            Personaje1.transform.localScale = new Vector3(2f, 2f, 0);
+            flechaMenos.SetActive(false);
 
 
-
-
-		//PERSONAJE2 
-		if (NuneroDePersonajeSeleccionado == 2) {
-			Personaje2.SetActive (true);
-
-		} else {
-			Personaje2.SetActive (false);
-
-		}
+        }
+        else {
+            Personaje1.transform.localScale = new Vector3(1f, 1f, 0);
+            flechaMenos.SetActive(true);
+        }
 
 
 
 
-		//PERSONAJE3
-		if (NuneroDePersonajeSeleccionado == 3) {
-			Personje3.SetActive (true);
+        //PERSONAJE2 
+        if (NuneroDePersonajeSeleccionado == 2)
+        {
+            Personaje2.transform.localScale = new Vector3(2f, 2f, 0);
 
-		} else {
-			Personje3.SetActive (false);
-				
-		}
+        }
+        else {
+    Personaje2 .transform.localScale = new Vector3(1f, 1f, 0);
+        }
+
+
+
+
+        //PERSONAJE3
+        if (NuneroDePersonajeSeleccionado == 3)
+        {
+            Personje3.transform.localScale = new Vector3(2f, 2f, 0);
+
+        }
+        else {
+            Personje3.transform.localScale = new Vector3(1f, 1f, 0);
+        }
+
 
 
 
 
 		//PERSONAJE4 
 		if (NuneroDePersonajeSeleccionado == 4) {
-			Personaje4.SetActive (true);
+            Personaje4.transform.localScale = new Vector3(2f, 2f, 0);
 
-		} else {
-			Personaje4.SetActive (false);
+        } else {
+            Personaje4.transform.localScale = new Vector3(1f, 1f, 0);
 
-		}
+        }
 			
 
 		//PERSONAJE 5 
 		if (NuneroDePersonajeSeleccionado == 5) {
-			Personaje5.SetActive (true);
-		
-		} else {
-			Personaje5.SetActive (false);
-		
-		}
+           Personaje5.transform.localScale = new Vector3(2f, 2f, 0);
+            flechaMas.SetActive(false);
+
+        } else {
+            Personaje5.transform.localScale = new Vector3(1f, 1f, 0);
+            flechaMas.SetActive(true);
+        }
 
 
 	
+      
 
 		}
 
